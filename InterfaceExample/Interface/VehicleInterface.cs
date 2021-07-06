f3341a308f26c037a713be5c855301a4de141c37
antonio.almeida <antonioalmeidausa@icloud.com>
2021-07-05 22:54:23 -0400

    Uploading Interface Construction

---
+++ InterfaceExample/Interface/VehicleInterface.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace InterfaceExample.Interface
+{
+    public interface IVehicleInterface
+    {
+        void AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage);
+
+        void DeleteVehicle(int stockNumber);
+
+        void UpdateVehicle(int stockNumber);
+
+        void SearchVehicleByStockNumber(int stockNumber);
+
+        void SearchAllVehicles();
+    }
+
+
+    public interface Store1Database : IVehicleInterface
+        {
+        void AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage);
+
+        void DeleteVehicle(int stockNumber);
+
+        void UpdateVehicle(int stockNumber);
+
+        void SearchVehicleByStockNumber(int stockNumber);
+
+        void SearchAllVehicles();
+    }
+
+    public interface Store2Database : IVehicleInterface
+    {
+        void AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage);
+
+        void DeleteVehicle(int stockNumber);
+
+        void UpdateVehicle(int stockNumber);
+
+        void SearchVehicleByStockNumber(int stockNumber);
+
+        void SearchAllVehicles();
+    }
+}
---
+++ InterfaceExample/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace InterfaceExample
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+
+        public int stockNumber { get; set; }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string Year { get; set; }
+
+        public string Mileage { get; set; }
+
+    }
+}
