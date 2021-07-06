a352e6dc9b84b603d03761799b12973364ac7a92
antonio.almeida <antonioalmeidausa@icloud.com>
2021-07-05 23:42:17 -0400

    Update interface

--- InterfaceExample/Interface/VehicleInterface.cs
+++ InterfaceExample/Interface/VehicleInterface.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace InterfaceExample.Interface
 {
     public interface IVehicleInterface
     {
-        void AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage);
+        public Vehicle AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage);
 
         void DeleteVehicle(int stockNumber);
 
@@ -11,33 +13,65 @@ namespace InterfaceExample.Interface
 
         void SearchVehicleByStockNumber(int stockNumber);
 
-        void SearchAllVehicles();
+        public List<Vehicle> SearchAllVehicles();
     }
 
 
-    public interface Store1Database : IVehicleInterface
+    public class Store1Database : IVehicleInterface
         {
-        void AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage);
+        
+        public Vehicle AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage)
+        {
+            throw new NotImplementedException();
+        }
 
-        void DeleteVehicle(int stockNumber);
+        public void DeleteVehicle(int stockNumber)
+        {
+            throw new NotImplementedException();
+        }
 
-        void UpdateVehicle(int stockNumber);
+        public void UpdateVehicle(int stockNumber)
+        {
+            throw new NotImplementedException();
+        }
 
-        void SearchVehicleByStockNumber(int stockNumber);
+        public List<Vehicle> SearchAllVehicles()
+        {
+            throw new NotImplementedException();
+        }
 
-        void SearchAllVehicles();
+        public void SearchVehicleByStockNumber(int stockNumber)
+        {
+            throw new NotImplementedException();
+        }
     }
 
-    public interface Store2Database : IVehicleInterface
+    public class Store2Database : IVehicleInterface
     {
-        void AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage);
+        public Vehicle AddVehicle(int stockNumber, string Make, string Model, string Year, string Mileage)
+        {
+            throw new NotImplementedException();
+        }
 
-        void DeleteVehicle(int stockNumber);
+        public void DeleteVehicle(int stockNumber)
+        {
+            throw new NotImplementedException();
+        }
 
-        void UpdateVehicle(int stockNumber);
+        public void UpdateVehicle(int stockNumber)
+        {
+            throw new NotImplementedException();
+        }
 
-        void SearchVehicleByStockNumber(int stockNumber);
+        public void SearchVehicleByStockNumber(int stockNumber)
+        {
+            throw new NotImplementedException();
+        }
 
-        void SearchAllVehicles();
+        public List<Vehicle> SearchAllVehicles()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
